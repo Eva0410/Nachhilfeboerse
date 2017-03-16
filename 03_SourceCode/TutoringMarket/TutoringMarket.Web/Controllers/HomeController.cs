62e648b4d1e5b37cc0677f35ec07158d404ff608
Eva0410 <evapuermayr@gmail.com>
2017-03-16 12:43:21 +0100

    Login geht

--- 03_SourceCode/TutoringMarket/TutoringMarket.Web/Controllers/HomeController.cs
+++ 03_SourceCode/TutoringMarket/TutoringMarket.Web/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TutoringMarket.Core.Contracts;
+using Authentication;
 
 namespace TutoringMarket.Web.Controllers
 {
@@ -17,10 +18,24 @@ namespace TutoringMarket.Web.Controllers
 
         public IActionResult Index()
         {
-            var li = _uow.TutorRepository.Get().ToList();
+            string user = "in130021";
+            string pw = "yuchdybu";
+
+            int? n = CheckLogin(user, pw).Result;
+
+            //var li = _uow.TutorRepository.Get().ToList();
             return View();
         }
-
+        async Task<int?> CheckLogin(string user, string pw)
+        {
+            int? num = null;
+            if (!String.IsNullOrEmpty(user) && !String.IsNullOrEmpty(pw))
+            {
+                SVSAuthenticationSoapClient client = new SVSAuthenticationSoapClient(SVSAuthenticationSoapClient.EndpointConfiguration.SVSAuthenticationSoap);
+                num = await client.CheckLdapSchuelerLoginEdvoAsync(user, pw);
+            }
+            return num;
+        }
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
